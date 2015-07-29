1a54bef83411836bfb96e0a50c7bffaeb09e2389
efrroul <frederick.rouleau@ericsson.com>
2015-07-29 16:08:47 -0400

    Login and Create Account (fix)

--- frontend/app/scripts/services/auth.js
+++ frontend/app/scripts/services/auth.js
@@ -1,15 +1,22 @@
 'use strict';
 
 angular.module('psJwtApp').service('auth', function ($http, authToken, API_URL, $state) {
-    var url = API_URL + 'login';
+    function authSuccessful(res) {
+        authToken.setToken(res.token);
+        $state.go('main');
+    }
     
     this.login = function(email, password) {
-        return $http.post(url, {
+        return $http.post(API_URL + 'login', {
             email: email, 
             password: password
-        }).success(function(res) {
-            authToken.setToken(res.token);
-            $state.go('main');
-        });
+        }).success(authSuccessful);
     }
+    
+    this.register = function(email, password) {
+        return $http.post(API_URL + 'register', {
+            email: email, 
+            password: password
+        }).success(authSuccessful);
+    }    
 });
